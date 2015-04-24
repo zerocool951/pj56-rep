fa572605e91c871421bd4b57da89c98cff020951
zerocool951 <zerocool951@gmail.com>
2015-04-24 16:42:23 +0200

    Eind punt

--- pj56-rep/src/frontEnd/WepApp.java
+++ pj56-rep/src/frontEnd/WepApp.java
@@ -35,27 +35,45 @@ public class WepApp extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String[] command = new String[2];
 
-		String[] s = req.getParameterValues("producttypes");
-		command[0] = "productsofatype";
+		String[] command = new String[2];
+		ArrayList<String> shoplist;
+		shoplist = (ArrayList<String>) req.getSession().getAttribute("cart");
+		if (shoplist == null) {
+			shoplist = new ArrayList<String>();
+		}
+		String fac = "";
+		fac = req.getParameter("factuur");
+		// String emptycart = "";
+		// fac = req.getParameter("empty");
+
+		String[] s = req
+				.getParameterValues(gatherer.constants.Constants.PRODUCT_TYPE_STATE);
+		command[0] = gatherer.constants.Constants.PRODUCT_OF_A_TYPE_STATE;
 		if (s == null) {
-			s = req.getParameterValues("productsofatype");
-			command[0] = "productattributes";
+			s = req.getParameterValues(gatherer.constants.Constants.PRODUCT_OF_A_TYPE_STATE);
+			command[0] = gatherer.constants.Constants.PRODUCT_ATTRIBUTES_STATE;
+			if (s != null) {
+				for (String paramvalue : s) {
+					shoplist.add(paramvalue);
+					req.getSession(true).setAttribute("cart", shoplist);
+				}
+
+			}
 			if (s == null) {
-				s = req.getParameterValues("productattributes");
-				command[0] = "producttypes";
-				// optie add shoppingcart session 
+				s = req.getParameterValues(gatherer.constants.Constants.PRODUCT_ATTRIBUTES_STATE);
+				command[0] = gatherer.constants.Constants.PRODUCT_TYPE_STATE;
+
 				req.getSession().getId();
-					
+
 			}
 		}
 		PrintWriter out = resp.getWriter();
 
 		// query product types
 		for (String paramvalue : s) {
-			out.println("<h1>" + paramvalue + "</h1>");
+			// out.println("<h1>" + fac + req.getSession().getId() + paramvalue
+			// + req.getSession().getAttribute("cart") + "</h1>");
 
 			ArrayList<String> gOutput = new ArrayList<String>();
 			Get nosql = new Get();
@@ -64,9 +82,57 @@ public class WepApp extends HttpServlet {
 			printPreamble(command[0], resp.getWriter());
 			for (String g : gOutput) {
 
-				out.println("<option>" + g + "</option>");
+				if (gOutput.indexOf(g) == 0) {
+					out.println("<option selected>" + g + "</option>");
+
+				}
+				out.println("<option >" + g + "</option>");
 
 			}
+
+			printPostForm(resp.getWriter());
+			/*
+			 * if (emptycart == "empty") { shoplist = new ArrayList<String>() ;
+			 * req.getSession(true).setAttribute("cart", shoplist);
+			 * 
+			 * }
+			 */
+
+			if (fac != null) {
+				float TotalPrice = 0;
+
+				out.println("<table>");
+				for (int i = 0; i < shoplist.size(); i++) {
+					command[0] = gatherer.constants.Constants.PRODUCT_PRICE_STATE;
+					command[1] = shoplist.get(i);
+					ArrayList<String> pOutput = new ArrayList<String>();
+					pOutput = nosql.start(command);
+
+					command[0] = gatherer.constants.Constants.PRODUCT_LINK_STATE;
+					command[1] = shoplist.get(i);
+					;
+					ArrayList<String> lOutput = new ArrayList<String>();
+					lOutput = nosql.start(command);
+					for (String p : pOutput) {
+						TotalPrice = TotalPrice + Float.parseFloat(p);
+						for (String l : lOutput) {
+							out.println("<tr><td><b>Product : </b>"
+									+ shoplist.get(i)
+									+ "</td><td><b>Price :</b> "
+									+ Float.parseFloat(p)
+									+ "</td><td><a href=\"" + l
+									+ "\">LINK</a></td></tr>");
+
+						}
+					}
+
+				}
+				out.println("<tr><td></td><td><b>TotalPrice : </b></td><td>"
+						+ TotalPrice + "</td></tr>");
+				out.println("</table>");
+
+			}
+
 			printPostamble(resp.getWriter());
 
 		}
@@ -80,21 +146,28 @@ public class WepApp extends HttpServlet {
 	}
 
 	private void printPreamble(String selectId, PrintWriter out) {
-		out.println("<html><head</head><body><form method=\"POST\" action=\"dowat\">");
-		out.println("<select name="+selectId+" id=" + selectId
+		out.println("<html><head> <meta http-equiv=\"Content-Type\" content=\"text/html;charset=ANSI\"> </head><body><img src=\"/pj56-rep/generic-logo.jpg\"> <form method=\"POST\" action=\"dowat\">");
+		out.println("<select name=" + selectId + " id=" + selectId
 				+ "  size=\"10\" style=\"width: 400px;\">");
 	}
 
 	private void printPostamble(PrintWriter out) {
+
+		out.println("</body></html>");
+	}
+
+	private void printPostForm(PrintWriter out) {
 		out.println("</select><button type=\"Submit\">post</button>");
-		out.println("</form></body></html>");
+		out.println("</select><button name=\"factuur\" value=\"factuur\" type=\"Submit\">factuur</button></form>");
+		// out.println("</select><button name=\"empty\" type=\"Submit\">empty shopping cart</button></form>");
+
 	}
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 
 		String[] s = new String[2];
-		s[0] = "producttypes";
+		s[0] = gatherer.constants.Constants.PRODUCT_TYPE_STATE;
 
 		try {
 
@@ -109,6 +182,7 @@ public class WepApp extends HttpServlet {
 						"<option>" + gOutput.get(i) + "</option>");
 			}
 
+			printPostForm(resp.getWriter());
 			printPostamble(resp.getWriter());
 
 		} catch (Exception e) {
